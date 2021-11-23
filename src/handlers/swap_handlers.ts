7416e9c62758f6574ee10abe0ac7c6f829f8630e
Lawrence Forman <me@merklejerk.com>
2021-11-23 17:10:50 -0500

    nicer quote output

--- src/handlers/swap_handlers.ts
+++ src/handlers/swap_handlers.ts
@@ -263,6 +263,8 @@ export class SwapHandlers {
             'allowanceTarget',
             'sellTokenToEthRate',
             'buyTokenToEthRate',
+            'maxSellAmount',
+            'minBuyAmount',
         );
 
         if (params.includePriceComparisons && quote.priceComparisonsReport) {
